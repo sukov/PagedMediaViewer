6516ca3e04216c635898c47939bf60b8043c9496
Gorjan Shukov <gorjan5@hotmail.com>
2025-10-01 19:13:37 +0200

    Update Swift package

--- Package.swift
+++ Package.swift
@@ -1,23 +1,24 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PagedMediaViewer",
     defaultLocalization: "en",
-    platforms: [.iOS(.v13)],
+    platforms: [
+        .iOS(.v13),
+    ],
     products: [
-        .library(
-            name: "PagedMediaViewer",
-            targets: ["PagedMediaViewer"]),
+        .library(name: "PagedMediaViewer", targets: ["PagedMediaViewer"]),
     ],
     dependencies: [],
     targets: [
-        .target(
-            name: "PagedMediaViewer",
-            dependencies: [],
-            path: "Source"
-        )
+        .target(name: "PagedMediaViewer", dependencies: [], path: "Source"),
+        .testTarget(name: "PagedMediaViewerTests",
+                    dependencies: ["PagedMediaViewer"],
+                    path: "Example/PagedMediaViewerTests"),
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
